060be0e8a987a6d1a36bde95aed58ff27bb85c27
Ashikzzzz <ashik.emon5.im@gmail.com>
2023-02-07 20:19:53 +0600

    almost done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const nodemailer = require("nodemailer");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 // middleware
 app.use(cors());
@@ -22,6 +23,25 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+// decode jwt
+// function verifyJWT(req, res, next) {
+//   const authHeader = req.headers.authorization;
+
+//   if (!authHeader) {
+//     return res.status(401).send({ message: "unauthorized access" });
+//   }
+//   const token = authHeader.split(" ")[1];
+
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+//     if (err) {
+//       return res.status(403).send({ message: "Forbidden access" });
+//     }
+//     console.log(decoded);
+//     req.decoded = decoded;
+//     next();
+//   });
+// }
+
 async function run() {
   try {
     const homesCollection = client.db("air-bnb").collection("homes");
@@ -29,12 +49,25 @@ async function run() {
     const bookingsCollection = client.db("air-bnb").collection("bookings");
     // console.log("first");
 
+    // Verify Admin
+    // const verifyAdmin = async (req, res, next) => {
+    //   const decodedEmail = req.decoded.email;
+    //   const query = { email: decodedEmail };
+    //   const user = await usersCollection.findOne(query);
+
+    //   if (user?.role !== "admin") {
+    //     return res.status(403).send({ message: "forbidden access" });
+    //   }
+    //   console.log("Admin true");
+    //   next();
+    // };
+
     // save user email and generate jwt
     app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
-      console.log("email", email);
+      // console.log("email", email);
       const user = req.body;
-      console.log("user info", user);
+      // console.log("user info", user);
       const filter = { email: email };
       const options = { upsert: true };
       const updatedDoc = {
@@ -48,7 +81,7 @@ async function run() {
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_JWT, {
         expiresIn: "1d",
       });
-      console.log(token);
+      // console.log(token);
       res.send({ result, token });
     });
 
@@ -82,7 +115,7 @@ async function run() {
       const email = req.params.email;
       const query = { email: email };
       const user = await usersCollection.findOne(query);
-      console.log(user.role);
+      // console.log(user?.role);
       res.send(user);
     });
 
@@ -122,12 +155,38 @@ async function run() {
       console.log(bookings);
       res.send(bookings);
     });
+
+    // create payment route
+    app.post("/payment-intent", async (req, res) => {
+      const price = req.body.price;
+      console.log(price);
+      const amount = parseFloat(price) * 100;
+      try {
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount,
+          currency: "usd",
+          payment_method_types: ["card"],
+        });
+        res.send({ clientSecret: paymentIntent.client_secret });
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
     // get all booking data
     app.get("/bookings", async (req, res) => {
       console.log(req.query);
       const query = {};
       const cursor = await bookingsCollection.find(query).toArray();
       res.send(cursor);
+    });
+
+    // delete booking
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await bookingsCollection.deleteOne(filter);
+      res.send(result);
     });
 
     // add a home
@@ -145,7 +204,51 @@ async function run() {
       const cursor = await homesCollection.find(query).toArray();
       res.send(cursor);
     });
-
+    // get home by single host
+    app.get("/homes", async (req, res) => {
+      let query = {};
+      console.log(req.query);
+      const email = req.query.email;
+      console.log(email);
+      if (email) {
+        query = { hostEmail: email };
+      }
+      const cursor = homesCollection.find(query);
+      const homes = await cursor.toArray();
+      res.send(homes);
+    });
+    // delete home data
+    app.delete("/homes/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await homesCollection.deleteOne(filter);
+      res.send(result);
+    });
+    // update home data
+    app.put("/home/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const update = req.body;
+      const option = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          location: update.location,
+          title: update.title,
+          price: update.price,
+          total_guest: update.total_guest,
+          bedrooms: update.bedrooms,
+          bathrooms: update.bathrooms,
+          description: update.description,
+        },
+      };
+      const result = await homesCollection.updateOne(
+        filter,
+        updatedDoc,
+        option
+      );
+      console.log(result);
+      res.send(result);
+    });
     // get a single home
     app.get("/home/:id", async (req, res) => {
       const id = req.params.id;
